0c742a352b0a2e29cc549d2be25dbc64790707a0
Samuel Tijani <omalitijani@gmail.com>
2020-08-12 21:45:20 +0100

    Add padding

--- lib/main.dart
+++ lib/main.dart
@@ -21,10 +21,16 @@ class DicePage extends StatelessWidget {
     return Row(
       children: <Widget>[
         Expanded(
-          child: Image.asset('images/dice1.png'),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.asset('images/dice1.png'),
+          ),
         ),
         Expanded(
-          child: Image.asset('images/dice1.png'),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.asset('images/dice1.png'),
+          ),
         ),
       ],
     );
