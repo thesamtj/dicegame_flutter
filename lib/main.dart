54ab02131ca5584d02d64e704e5f29f6f158dcbc
Samuel Tijani <omalitijani@gmail.com>
2020-08-13 19:34:25 +0100

    Change to stateful

--- lib/main.dart
+++ lib/main.dart
@@ -15,10 +15,15 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
   @override
   Widget build(BuildContext context) {
-    var leftDiceNumber = 4;
+    int leftDiceNumber = 4;
 
     return Center(
       child: Row(
